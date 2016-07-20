f1db76f0759be789dbe7e494950aa5dbef8645ce
Ray Meibaum <rmeibaum@me.com>
2016-07-20 15:38:21 +0000

    finished updating for github portfolio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 "use strict";
+$(function() {
+  var $results = $('#results');
+  $('#searchForm').submit(function(event) {
+    event.preventDefault();
+    var q = $('#searchBox').val();
+    $.getJSON("https://en.wikipedia.org/w/api.php?callback=?", {
+        srsearch: q,
+        action: "query",
+        list: "search",
+        format: "json"
+      },
+      function(data) {
+        $results.empty();
+        $.each(data.query.search, function(i, item) {
+          $results.append('<a href="https://en.wikipedia.org/wiki/' + item.title + '"><div class="media"><div class="media-body"><h4 class="media-heading">' + item.title + '</h4></a>' + item.snippet + '</div></div>');
+        });
+      });
+  });
+})
